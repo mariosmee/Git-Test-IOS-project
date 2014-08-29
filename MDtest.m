59bd90eb7b049940a39b46bcdfdef346b8cce1ff
Mario Donato Silva <mariosilva@u-Shouldnt.local>
2014-08-29 11:46:38 -0300

    added Test Class

---
+++ MDtest.m
@@ -0,0 +1,13 @@
+//
+//  MDtest.m
+//  Git Test
+//
+//  Created by Mario Donato Silva on 29/08/14.
+//  Copyright (c) 2014 Donato Development. All rights reserved.
+//
+
+#import "MDtest.h"
+
+@implementation MDtest
+
+@end
