3e7d78491c0b551ec0b84f928c23837be2863b96
Mario Donato Silva <mariosilva@u-Shouldnt.local>
2014-08-29 22:23:38 -0300

    added Other

---
+++ Git Test/MDOther.h
@@ -0,0 +1,13 @@
+//
+//  MDOther.h
+//  Git Test
+//
+//  Created by Mario Donato Silva on 29/08/14.
+//  Copyright (c) 2014 Donato Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDOther : NSObject
+
+@end
---
+++ Git Test/MDOther.m
@@ -0,0 +1,13 @@
+//
+//  MDOther.m
+//  Git Test
+//
+//  Created by Mario Donato Silva on 29/08/14.
+//  Copyright (c) 2014 Donato Development. All rights reserved.
+//
+
+#import "MDOther.h"
+
+@implementation MDOther
+
+@end
